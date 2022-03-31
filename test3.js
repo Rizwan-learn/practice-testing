10dc64789dc5b1959a8c851cc5142db2ab8219b8
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-03-31 14:30:52 +0100

    test 3

---
+++ test3.js
@@ -0,0 +1,20 @@
+class calculator {
+    add(a,b){
+        let result = a+b;
+        return result;
+    }
+    minus(a,b){
+        let result = a-b;
+        return result;
+    }
+    multiply(a,b){
+        let result = a*b;
+        return result;
+    }
+    div(a,b){
+        let result = a/b;
+        return result;
+    }
+}
+
+module.exports = calculator;
